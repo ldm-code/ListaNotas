c31252ab62de1dc90d51365f4a0513631be55dcd
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-09-10 18:24:03 -0300

    Update banco_aluno.sql

--- dados/banco_aluno.sql
+++ dados/banco_aluno.sql
@@ -3,7 +3,8 @@
 -- Host: localhost    Database: alunos
 -- ------------------------------------------------------
 -- Server version	8.0.43
-
+CREATE DATABASE alunos;
+USE alunos;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
